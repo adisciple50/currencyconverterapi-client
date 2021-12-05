fb1f3242644693e9036563fdd06c15ba371c0b7e
Jason Crockett <jesusislord33333@outlook.com>
2021-12-05 20:41:09 +0000

    before microsoft pain

--- currency_converter_api.rb
+++ currency_converter_api.rb
@@ -12,30 +12,30 @@ class CurrencyConverterApi
     @pairs_to_query = []
     @compact = Ultra
     @api_key = ENV['currency_converter_api']
-    @options = { query: { apiKey:@api_key } }
+    @request = { query: { apiKey:@api_key } }
   end
 
   def convert
-    self.class.get('/api/v7/convert',@options['query'].merge({q:@pairs_to_query,compact:@compact}))
+    self.class.get('/api/v7/convert', @request['query'].merge({ q:@pairs_to_query, compact:@compact}))
   end
 
   def currencies
-    self.class.get('/api/v7/currencies',@options)
+    self.class.get('/api/v7/currencies', @request)
   end
 
   def countries
-    self.class.get('/api/v7/countries',@options)
+    self.class.get('/api/v7/countries', @request)
   end
 
   def single_historical_date(date:Date)
-    self.class.get('/api/v7/convert',@options['query'].merge({q:@pairs_to_query,compact:@compact,date:date.strftime("%Y-%m-%d")}))
+    self.class.get('/api/v7/convert', @request['query'].merge({ q:@pairs_to_query, compact:@compact, date:date.strftime("%Y-%m-%d")}))
   end
 
   def historical_date_range(date:Date,end_date:Date)
     if date > date + 365
       raise('Date range is bigger than 365 days')
     end
-    self.class.get('/api/v7/convert',@options['query'].merge({q:@pairs_to_query,compact:@compact,date:date.strftime("%Y-%m-%d"),endDate:date.strftime("%Y-%m-%d")}))
+    self.class.get('/api/v7/convert', @request['query'].merge({ q:@pairs_to_query, compact:@compact, date:date.strftime("%Y-%m-%d"), endDate:date.strftime("%Y-%m-%d")}))
   end
 end
 end
--- lib/currencyconverterapi.rb
+++ lib/currencyconverterapi.rb
@@ -2,7 +2,55 @@
 
 require_relative "currencyconverterapi/version"
 
-module Currencyconverterapi
+module CurrencyConverterApi
+  require 'httparty'
+  require 'date'
+
   class Error < StandardError; end
-  # Your code goes here...
+
+  class DedicatedClient
+    include HTTParty
+    Ultra = 'ultra'
+    attr_accessor :pairs_to_query,:compact
+    attr_reader :request
+
+    base_uri 'https://tradeup.currconv.com'
+
+    @@request = { query: { apiKey:ENV['currency_converter_api'] } }
+    @@compact = Ultra
+    @@pairs_to_query = []
+
+    def initialize
+      @pairs_to_query = []
+      @compact = Ultra
+      @@request = { query: { apiKey:ENV['currency_converter_api'] } }
+    end
+
+    def self.server(uri)
+      base_uri uri
+    end
+
+    def self.convert(pairs_array)
+      get('/api/v7/convert', @@request['query'].merge({ q:pairs_array, compact:@compact})).body
+    end
+
+    def self.currencies
+      get('/api/v7/currencies',@request)
+    end
+
+    def self.countries
+      get('/api/v7/countries', @request)
+    end
+
+    def self.single_historical_date(date:Date)
+      get('/api/v7/convert', @request['query'].merge({ q:@pairs_to_query, compact:@compact, date:date.strftime("%Y-%m-%d")}))
+    end
+
+    def self.historical_date_range(date:Date,end_date:Date)
+      if date > date + 365
+        raise('Date range is bigger than 365 days')
+      end
+      get('/api/v7/convert', @request['query'].merge({ q:@pairs_to_query, compact:@compact, date:date.strftime("%Y-%m-%d"), endDate:date.strftime("%Y-%m-%d")}))
+    end
+  end
 end
